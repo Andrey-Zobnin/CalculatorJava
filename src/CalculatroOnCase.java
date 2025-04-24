20f508ace69e19699a567b1448977e1e0af48918
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-04-24 11:25:12 +0300

    console log have a color now add try-except

--- src/CalculatroOnCase.java
+++ src/CalculatroOnCase.java
@@ -1,44 +1,58 @@
-import java.lang.*;
 import java.util.Scanner;
 
 public class CalculatroOnCase {
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+
+    private static final char ADD = '+';
+    private static final char SUBTRACT = '-';
+    private static final char MULTIPLY = '*';
+    private static final char DIVIDE = '/';
+
     public static void main(String[] args) {
-        double num1 = 0, num2 = 0;
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter nums:");
-        num1 = scan.nextDouble();
-        num2 = scan.nextDouble();
-        System.out.println("Enter the operator (+,-,*,/):");
+        try {
+            System.out.println(YELLOW + "Enter nums:" + RESET);
+            double num1 = scan.nextDouble();
+            double num2 = scan.nextDouble();
 
-        char operation = scan.next().charAt(0);
-        double currentOperation = 0;
+            System.out.println(YELLOW + "Enter the operator (+,-,*,/):" + RESET);
+            char operation = scan.next().charAt(0);
 
-        switch (operation) {
-            case '+':
-                currentOperation = num1 + num2;
-                break;
+            double result = calculate(num1, num2, operation);
+            displayResult(num1, num2, operation, result);
 
-            case '-':
-                currentOperation = num1 - num2;
-                break;
-
-            case '*':
-                currentOperation = num1 * num2;
-                break;
-
-            case '/':
-                currentOperation = num1 / num2;
-                break;
-
-            default:
-                System.out.println("Wrong.. 1");
-                System.out.println("Stop process");
-                break;
+        } catch (Exception e) {
+            System.out.println(RED + "Invalid input! Please enter numbers only." + RESET);
+        } finally {
+            scan.close();
         }
+    }
 
-        System.out.println("Result: ");
-        System.out.println();
-        System.out.println(num1 + " " + operation + " " + num2 + " = " + currentOperation);
+    private static double calculate(double num1, double num2, char operation) {
+        switch (operation) {
+            case ADD:
+                return num1 + num2;
+            case SUBTRACT:
+                return num1 - num2;
+            case MULTIPLY:
+                return num1 * num2;
+            case DIVIDE:
+                if (num2 == 0) {
+                    throw new ArithmeticException("Division by zero!");
+                }
+                return num1 / num2;
+            default:
+                throw new IllegalArgumentException("Invalid operator!");
+        }
+    }
+
+    private static void displayResult(double num1, double num2, char operation, double result) {
+        System.out.println(GREEN + "Result: " + RESET);
+        System.out.println(BLUE + num1 + " " + operation + " " + num2 + " = " + result + RESET);
     }
 }
